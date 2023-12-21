607e7d5c900782504f440cbde5689eb4f8459c64
Aahil <136536751+AahilSomethin@users.noreply.github.com>
2023-12-21 11:05:02 +0500

    sorta connecting signup and login

--- code/gui.py
+++ code/gui.py
@@ -1,6 +1,6 @@
+import tkinter as tk
+from tkinter import filedialog
 from email.mime import image
-import tkinter
-import tkinter.messagebox
 import customtkinter
 
 customtkinter.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
@@ -24,22 +24,25 @@ class Main(customtkinter.CTk):
         self.sidebar_frame = customtkinter.CTkFrame(self, width=140, corner_radius=0)
         self.sidebar_frame.grid(row=0, column=0, rowspan=4, sticky="nsew")
         self.sidebar_frame.grid_rowconfigure(4, weight=1)
-        self.logo_label = customtkinter.CTkLabel(self.sidebar_frame, text="The Horizon Cinemas", font=customtkinter.CTkFont(size=15, weight="bold"))
+        self.logo_label = customtkinter.CTkLabel(self.sidebar_frame, text="The Horizon Cinemas",
+                                                  font=customtkinter.CTkFont(size=15, weight="bold"))
         self.logo_label.grid(row=0, column=0, padx=20, pady=(20, 10))
-        self.sidebar_button_1 = customtkinter.CTkButton(self.sidebar_frame, command=self.login)
+        self.sidebar_button_1 = customtkinter.CTkButton(self.sidebar_frame, text="Login", command=self.login)
         self.sidebar_button_1.grid(row=1, column=0, padx=20, pady=10)
-        self.sidebar_button_2 = customtkinter.CTkButton(self.sidebar_frame, command=self.signup)
+        self.sidebar_button_2 = customtkinter.CTkButton(self.sidebar_frame, text="Signup", command=self.signup)
         self.sidebar_button_2.grid(row=2, column=0, padx=20, pady=10)
         self.sidebar_button_3 = customtkinter.CTkButton(self.sidebar_frame, command=self.logout)
         self.sidebar_button_3.grid(row=3, column=0, padx=20, pady=10)
         self.appearance_mode_label = customtkinter.CTkLabel(self.sidebar_frame, text="Appearance Mode:", anchor="w")
         self.appearance_mode_label.grid(row=5, column=0, padx=20, pady=(10, 0))
-        self.appearance_mode_optionemenu = customtkinter.CTkOptionMenu(self.sidebar_frame, values=["Light", "Dark", "System"],
+        self.appearance_mode_optionemenu = customtkinter.CTkOptionMenu(self.sidebar_frame,
+                                                                       values=["Light", "Dark", "System"],
                                                                        command=self.change_appearance_mode_event)
         self.appearance_mode_optionemenu.grid(row=6, column=0, padx=20, pady=(10, 10))
         self.scaling_label = customtkinter.CTkLabel(self.sidebar_frame, text="UI Scaling:", anchor="w")
         self.scaling_label.grid(row=7, column=0, padx=20, pady=(10, 0))
-        self.scaling_optionemenu = customtkinter.CTkOptionMenu(self.sidebar_frame, values=["80%", "90%", "100%", "110%", "120%"],
+        self.scaling_optionemenu = customtkinter.CTkOptionMenu(self.sidebar_frame,
+                                                               values=["80%", "90%", "100%", "110%", "120%"],
                                                                command=self.change_scaling_event)
         self.scaling_optionemenu.grid(row=8, column=0, padx=20, pady=(10, 20))
 
@@ -47,7 +50,8 @@ class Main(customtkinter.CTk):
         self.entry = customtkinter.CTkEntry(self, placeholder_text="What do you wish to find")
         self.entry.grid(row=3, column=1, columnspan=2, padx=(20, 0), pady=(20, 20), sticky="nsew")
 
-        self.main_button_1 = customtkinter.CTkButton(master=self, fg_color="transparent", border_width=2, text_color=("gray10", "#DCE4EE"), command=self.search)
+        self.main_button_1 = customtkinter.CTkButton(master=self, fg_color="transparent", border_width=2,
+                                                      text_color=("gray10", "#DCE4EE"), command=self.search)
         self.main_button_1.grid(row=3, column=3, padx=(20, 20), pady=(20, 20), sticky="nsew")
 
         # create textbox
@@ -78,21 +82,6 @@ class Main(customtkinter.CTk):
         self.seg_button_1.configure(values=["All", "New", "cartoon", "Action", "Comedy", "Drama"])
         self.seg_button_1.set("All")
 
-                # Load an image
-        #image_path = "path/to/your/image.jpg"  # Replace with the actual path to your image
-        #img = Image.open(image_path)
-        #img = img.resize((20, 20), Image.ANTIALIAS)  # Adjust the size as needed
-
-        # Convert the image to Tkinter PhotoImage format
-        #img_tk = ImageTk.PhotoImage(img)
-
-        # Update the main button with the image
-        #self.main_button_1 = customtkinter.CTkButton(master=self, fg_color="transparent", border_width=2, text_color=("gray10", "#DCE4EE"), image=img_tk, compound="center")
-       #self.main_button_1.grid(row=3, column=3, padx=(20, 20), pady=(20, 20), sticky="nsew")
-
-        # Save a reference to the image to prevent garbage collection
-        #self.main_button_1.image = img_tk
-
     def search(self):
         # Get the search query from the entry
         search_query = self.entry.get()
@@ -115,22 +104,52 @@ class Main(customtkinter.CTk):
         customtkinter.set_widget_scaling(new_scaling_float)
 
     def login(self):
-        new_window = customtkinter.CTkToplevel(self)
-        new_window.title("Signup/login")
-        new_window.geometry(f"{500}x{580}")
+        file_path = filedialog.askopenfilename(filetypes=[("Python files", "login and signup.py")])
+    
+        if file_path:
+            with open(file_path, 'r') as file:
+                content = file.read()
+                # You can do something with the content, like displaying it in a text widget
+                print(content)
 
     def signup(self):
-        new_window = customtkinter.CTkToplevel(self)
-        new_window.title("Signup/login")
-        new_window.geometry(f"{500}x{580}")
+        file_path = filedialog.askopenfilename(filetypes=[("Python files", "login and signup.py")])
+        
+        if file_path:
+            with open(file_path, 'r') as file:
+                content = file.read()
+                # You can do something with the content, like displaying it in a text widget
+                print(content)
 
     def logout(self):
-        print("works logout")
+        # Add your logout logic here
+
+        # Example: Clear the entry and textbox content
+        self.entry.delete(0, tk.END)
+        self.textbox.delete("1.0", tk.END)
+
+        # Example: Enable buttons that should be enabled when logged out
+        self.sidebar_button_1.configure(state="enabled")
+        self.sidebar_button_2.configure(state="enabled")
+        self.sidebar_button_3.configure(state="disabled")
+
+        # Example: Change the appearance mode to default (System)
+        customtkinter.set_appearance_mode("System")
+
+        # Example: Reset the UI scaling to default (100%)
+        customtkinter.set_widget_scaling(1.0)
+
+        # Example: Stop and reset the progress bar
+        self.progressbar_1.stop()
+        self.progressbar_1.configure(mode="indeterminate")
+
+        # Example: Set default values for appearance mode and scaling optionmenus
+        self.appearance_mode_optionemenu.set("System")
+        self.scaling_optionemenu.set("100%")
+
+        print("Logout button clicked")
 
 
 if __name__ == "__main__":
     app = Main()
     app.mainloop()
-
-    app.mainloop()
-    
